ba177ae805822e2ada7469fc5ec48f3d2bf22fa1
Bert Spaan <bert.spaan@gmail.com>
2013-08-14 16:16:33 +0200

    Loads CORA data from local files

--- importers/cora/cora.rb
+++ importers/cora/cora.rb
@@ -1,99 +1,36 @@
-require '/var/www/csdk_cms/current/utils/citysdk_api.rb'
+# encoding: UTF-8
+require 'citysdk'
 
-# CORA
+credentials = '/var/www/citysdk/shared/config/cdkpw.json'
+pw = File.exists?(credentials) ? JSON.parse(File.read(credentials)) : nil
+$email = ARGV[0] || (pw ? pw['email'] : nil) || 'citysdk@waag.org'
+$passw = ARGV[1] || (pw ? pw[$email]  : nil) || ''
+$host  = ARGV[2] || (pw ? pw['host']  : nil) || 'api.dev'
 
-pw = JSON.parse(File.read('/var/www/citysdk/shared/config/cdkpw.json')) if File.exists?('/var/www/citysdk/shared/config/cdkpw.json')
-$email = ARGV[0] || 'citysdk@waag.org'
-$password = ARGV[1] || (pw ? pw[$email] : '')
+layers = [
+  "projecten", 
+  "omleidingen",
+  "belemmeringen"
+]
 
+begin  
+  layers.each { |layer|    
+    
+    importer = CitySDK::Importer.new ({
+        :file_path => "#{layer}.geojson", 
+        :host => $host, 
+        :email => $email,
+        :passw => $passw,
+        :layername => "divv.cora.#{layer}"
+      })
+      
+    puts JSON.pretty_generate(importer.params)
+    
+    importer.params[:srid] = 28992
+    
+    importer.doImport do |params| 
+      
+    end
 
-begin 
-$api = CitySDK_API.new($email, $password)
-
-# cora_host = "https://dl.dropboxusercontent.com"
-# cora_path = "/u/12905316/citysdk/cora/%s_Amsterdam_GeoJson.json"
-
-cora_host = "http://dev.citysdk.waag.org"
-cora_path = "/%s_Amsterdam_GeoJson.json"
-
-if $api.authenticate == false 
-  puts "Auth failure with citysdk."
-  exit!
-end
-
-layers = {
-  # "belemmeringen"=>"PROJECT_ID",
-  # "omleidingen"=>"OMLEIDING_ID",
-  "projecten" => "WIORPR_ID"
-}
-
-def downkeys(h) 
-  hh = {}
-  h.each do |k,v|
-    hh[k.downcase] = v
-  end
-  return hh
-end
-  
-
-
-$api.batch_size = 1
-
-count = 10000
- 
-layers.each { |layer, id| 
- 
-   ###### Download CORA data ######
-   puts "Downloading CORA data: #{layer}..."
- 
-   cora_conn = Faraday.new(:url => cora_host)
-   response = cora_conn.get cora_path % [layer]
-   results = JSON.parse(response.body)
- 
-
-   $api.set_layer("divv.cora.#{layer}")
- 
-   $api.set_createTemplate( {
-     :create => {
-       :params => {
-         :create_type => "create",
-         :srid => 28992
-       }
-     }
-   } )
-
-   results['features'].each do |f|
-
-     count += 1
-     begin
-       $api.create_node(
-         {
-           :id => f['properties'][id].to_s,       
-           # :id => count.to_s,       
-           :geom => f['geometry'],
-           :data => downkeys(f['properties'])
-         })
-     rescue
-     end
-   end
-   
-   # puts "Received #{nodes.length} nodes.."
-   # $stderr.puts "Received #{nodes.length} nodes.."
- 
- 
-   # puts JSON.pretty_generate(   
-   # nodes.map do |n|
-   #   {
-   #     :id => n[:id]
-   #   }
-   # end
-   # )
-   
- 
-}
-
-ensure 
-
-$api.release
-
+  }
 end
