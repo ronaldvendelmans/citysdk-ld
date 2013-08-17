538107223aa4071bcbd043062c5c84eb84aa191a
Tom Demeyer <tom@wallaby.fritz.box>
2013-08-17 16:06:30 +0200

    redirect & entities

--- rdf/rdfsrc.rb
+++ rdf/rdfsrc.rb
@@ -1,9 +1,21 @@
 require 'sinatra'
+require 'rdf/turtle'
+include RDF
+
+
+def load_ttl
+  @ents = []
+  @graph = Graph.load("./public/citysdk.ttl", :format => :ttl)
+  @graph.each do |t|
+    s = t.subject.to_s.gsub('http://rdf.citysdk.eu/','')
+    @ents << s if not (@ents.include?(s) or s == '')
+  end
+end
 
 class CSDKRdf < Sinatra::Base
   
-  
   before do
+    load_ttl
     case request.env['HTTP_ACCEPT']
       when 'application/rdf+xml'
         @type = 'application/rdf+xml'
@@ -14,7 +26,7 @@ class CSDKRdf < Sinatra::Base
 
   after do
   end
-
+  
   get '/' do
     case @type
       when 'application/rdf+xml'
@@ -23,9 +35,22 @@ class CSDKRdf < Sinatra::Base
         send_file './public/citysdk.ttl', :type => @type, :disposition => :inline
       else
         send_file './public/citysdk.ttl', :type => @type, :disposition => :inline
-      end
+    end
+  end
+  
+  get '/asd/*' do 
+    redirect "http://api.citysdk.waag.org/#{params[:splat][0]}", 303
   end
 
-end
+  get '/:entity' do 
+    if @ents.include?(params[:entity])
+      [200,{},"Found: #{params[:entity]}" ]
+    else
+      [404,{},"Not found: #{params[:entity]}" ]
+    end
+  end
 
+  
+
+end
 
