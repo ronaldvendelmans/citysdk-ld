01ac3e53a887f2f8805f36cefd3a833e942e37ac
Bert Spaan <bert.spaan@gmail.com>
2014-02-25 14:22:30 +0100

    only layers when layers contains data

--- server/serializers/geojson.rb
+++ server/serializers/geojson.rb
@@ -15,16 +15,17 @@ class GeoJSONSerializer < Serializer::Base
   
   def self.nodes
     @objects.each do |node|
-      @geojson[:features] << {
+      feature = {
         type: "Feature",
         properties: {            
           cdk_id: node[:cdk_id],
           name: node[:name],
-          layer: node[:layer],
-          layers: node[:layers]
+          layer: node[:layer]          
         },
         geometry: node[:geom]
       }
+      feature[:properties][:layers] = node[:layers] if node.has_key? :layers and node[:layers] 
+      @geojson[:features] << feature
     end    
   end
 
