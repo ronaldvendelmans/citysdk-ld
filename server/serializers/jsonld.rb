5c35a3ba9c79f8edc058ca08690159bfa0df9209
Bert Spaan <bert.spaan@gmail.com>
2014-03-14 16:22:50 +0100

    first version of jsonld layer serialization

--- server/serializers/jsonld.rb
+++ server/serializers/jsonld.rb
@@ -4,18 +4,35 @@ class JSONLDSerializer < GeoJSONSerializer
   #https://code.google.com/p/linked-data-api/wiki/API_Viewing_Resources#Page_Description
   
   def self.end
-    @result = {
-      :@context => create_context,
-      :@id => @result[:meta][:url],
-      :@type => "cdk:APIResult",
-    }.merge @result
-
     # @result[:meta] = {
     #   :"@type" => "cdk:ApiResultMeta"
     # }.merge @result[:meta]
     
+    case @type    
+    when :node, :nodes
+      @result = {
+        :@context => create_node_context,
+        :@id => @result[:meta][:url],
+        :@type => "cdk:APIResult",
+      }.merge @result      
+      
+      jsonld_nodes
+    when :layer, :layers      
+      @result = {
+        :@context => create_layer_context,
+        :@id => @result[:meta][:url],
+        :@type => "cdk:APIResult",
+      }.merge @result      
+      
+      jsonld_layers
+    end
+    
+    super
+  end
+  
+  def self.jsonld_nodes
     first_feature = true
-    @result[:features].map! do |feature| 
+    @result[:features].map! do |feature|      
       cdk_id = feature[:properties][:cdk_id]
       feature[:properties] = {
         :@id => "cdk:objects/#{cdk_id}"
@@ -31,8 +48,7 @@ class JSONLDSerializer < GeoJSONSerializer
         
         context = "http://api.citysdk.waag.org/layers/#{l}/fields"
         if first_feature
-          c = create_layer_context @layers[l]
-          context = c if c
+          context = @layers[l][:context] if @layers[l][:context]
         end
         
         layer[:data] = {
@@ -51,11 +67,23 @@ class JSONLDSerializer < GeoJSONSerializer
         :"@type" => "cdk:Object",
       }.merge feature
     end
-    
-    super
   end
   
-  def self.create_context
+  def self.jsonld_layers
+    first_feature = true
+    @result[:features].map! do |feature|
+      feature[:properties] = {
+        :@id => "cdk:layers/#{feature[:properties][:name]}"
+      }.merge feature[:properties]
+
+      {
+        :"@id" => "cdk:layers/#{feature[:properties][:name]}",
+        :"@type" => ["cdk:Layer", "dcat:Dataset"]
+      }.merge feature
+    end
+  end  
+  
+  def self.create_node_context
     # TODO: set correct base, and use config.json
     {      
       :@base => "http://rdf.citysdk.eu/ams/",
@@ -77,11 +105,14 @@ class JSONLDSerializer < GeoJSONSerializer
     }
   end
   
-  def self.create_layer_context(layer)
-    if layer[:context]
-      return layer[:context]
-    end
-    nil
+  def self.create_layer_context    
+    {
+      :@base => "http://rdf.citysdk.eu/ams/",
+      :title => "dct:title",
+      :features => "cdk:apiResult",
+      :properties => "_:properties",
+      :imported_at => "dct:modified"
+    }
   end
 
 end
