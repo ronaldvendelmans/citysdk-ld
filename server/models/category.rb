1b86dd44674ae2a69f5ffa464e58e59303116080
Bert Spaan <bert.spaan@gmail.com>
2014-04-02 16:33:25 +0200

    created new model class

--- server/models/category.rb
+++ server/models/category.rb
@@ -4,15 +4,3 @@ class Category < Sequel::Model
 	plugin :validation_helpers
 end
 
-class Prefix < Sequel::Model(:ldprefix)
-	plugin :validation_helpers
-end
-
-class OSMProps < Sequel::Model(:osmprops)
-end
-
-
-class LayerProperty < Sequel::Model(:ldprops)
-end
-
-
---
+++ server/models/layerproperty.rb
@@ -0,0 +1,22 @@
+require "sequel/model"
+
+class LayerProperty < Sequel::Model(:ldprops)
+  def self.make_hash(l)
+    {
+      id: l[:key],
+      lang: l[:lang],
+      type: l[:type],
+      unit: l[:unit],
+      description: l[:descr],
+      eqprop: l[:eqprop]
+    }.delete_if{ |k, v| not v or v == '' }
+  end
+end
+
+class Prefix < Sequel::Model(:ldprefix)
+	plugin :validation_helpers
+end
+
+class OSMProps < Sequel::Model(:osmprops)
+  
+end
